46a758f8655e2c04fb98f7af4e526e76eb4fca29
unknown <macjay314@gmail.com>
2020-06-10 16:26:42 -0700

    Add Rating Views

--- Controllers/PlacesController.cs
+++ Controllers/PlacesController.cs
@@ -16,6 +16,14 @@ namespace TravelClient.Controllers
       return View(allPlaces);
     }
 
+    [HttpPost]
+    public IActionResult Index(Place place)
+    {
+      Place.Post(place);
+      return RedirectToAction("Index");
+    }
+
+
     public IActionResult Details(int id)
     {
       var thisPlace = Place.GetDetails(id);
@@ -29,6 +37,29 @@ namespace TravelClient.Controllers
           }
         }
       return View(thisPlace);
-    }  
+    }
+
+    public IActionResult Edit(int id)
+    {
+      var place = Place.GetDetails(id);
+      return View(place);
+    }
+
+    [HttpPost]
+    public IActionResult Details(int id, Place place)
+    {
+      place.PlaceId = id;
+      Place.Put(place);
+      return RedirectToAction("Details", id);
+    }
+
+    public IActionResult Delete(int id)
+    {
+      Place.Delete(id);
+      return RedirectToAction("Index");
+    }
+    
+
+
   }
 }
--- Controllers/RatingsController.cs
+++ Controllers/RatingsController.cs
@@ -11,11 +11,41 @@ namespace TravelClient.Controllers
   public class RatingsController : Controller
   {
 
-
     public IActionResult Details(int id)
     {
-      var thisPlace = Place.GetDetails(id);
-      return View(thisPlace);
+      var thisRating = Rating.GetDetails(id);
+      return View(thisRating);
+    }
+
+    [HttpPost]
+    public IActionResult Rate(int id, Rating rating)
+    {
+      var thisPlace = Place.GetPlaces.Where(p => p.PlaceId == id);
+      ViewBag.Place = thisPlace;
+      Rating.Post(rating);
+      return RedirectToAction("Places/{placeId}");
     }  
+
+    public IActionResult Edit(int id)
+    {
+      var rating = Rating.GetDetails(id);
+      return View(rating);
+    }
+
+    [HttpPost]
+    public IActionResult Details(int id, Rating rating)
+    {
+      rating.RatingId = id;
+      Rating.Put(rating);
+      return RedirectToAction("Details", id);
+    }
+
+    public IActionResult Delete(int id)
+    {
+      Rating.Delete(id);
+      return RedirectToAction("Index");
+    }
+
+
   }
 }
